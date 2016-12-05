22890bbd50f9c15764557fe8eed8a69db58c53d5
Tosin <tosin.yusuf@digital.bis.gov.uk>
2016-12-05 11:26:44 +0000

    Added LITECommon.initialiseSelectionButtons global function.

--- public/template/javascripts/lite-common.js
+++ public/template/javascripts/lite-common.js
@@ -1,6 +1,11 @@
-$(document).ready(function () {
+var LITECommon = LITECommon || {};
+
+LITECommon.initialiseSelectionButtons = function () {
   // Use GOV.UK selection-buttons.js to set selected
   // and focused states for block labels
   var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']")
   new GOVUK.SelectionButtons($blockLabels) // eslint-disable-line
-})
+};
+
+
+$(document).ready(LITECommon.initialiseSelectionButtons())
