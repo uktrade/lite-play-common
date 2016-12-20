19abfdcd5c891aa692718983cd8a628dbfc30621
Tosin <tosin.yusuf@digital.bis.gov.uk>
2016-12-20 12:16:54 +0000

    Removed Named annotations from CountryServiceClient.

--- app/components/common/client/CountryServiceClient.java
+++ app/components/common/client/CountryServiceClient.java
@@ -3,7 +3,6 @@ package components.common.client;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
-import com.google.inject.name.Named;
 import components.common.logging.CorrelationId;
 import models.common.Country;
 import play.Logger;
@@ -25,37 +24,36 @@ public class CountryServiceClient {
 
   @Inject
   public CountryServiceClient(HttpExecutionContext httpExecutionContext,
-                              WSClient wsClient,
-                              @Named("countryServiceAddress") String countryServiceAddress,
-                              @Named("countryServiceTimeout") int countryServiceTimeout,
-                              ObjectMapper objectMapper) {
+                              WSClient wsClient, String countryServiceUrl,
+                              int countryServiceTimeout, ObjectMapper objectMapper) {
     this.httpExecutionContext = httpExecutionContext;
     this.wsClient = wsClient;
-    this.countryServiceUrl = countryServiceAddress;
+    this.countryServiceUrl = countryServiceUrl;
     this.countryServiceTimeout = countryServiceTimeout;
     this.objectMapper = objectMapper;
   }
 
   public CompletionStage<List<Country>> getCountries() {
     return wsClient.url(countryServiceUrl)
-        .withRequestFilter(CorrelationId.requestFilter)
-        .setRequestTimeout(countryServiceTimeout)
-        .get().handleAsync((result, error) -> {
-          if (error != null) {
-            Logger.error("Country service client failure.", error);
-          } else if (result.getStatus() != 200) {
-            Logger.error("Country service error - {}", result.getBody());
-          } else {
-            try {
-              String json = result.asJson().toString();
-              return objectMapper.readValue(json, new TypeReference<List<Country>>() {});
-            } catch (IOException e) {
-              Logger.error("Failed to parse Country service response as JSON.", e);
-            }
+      .withRequestFilter(CorrelationId.requestFilter)
+      .setRequestTimeout(countryServiceTimeout)
+      .get().handleAsync((result, error) -> {
+        if (error != null) {
+          Logger.error("Country service client failure.", error);
+        } else if (result.getStatus() != 200) {
+          Logger.error("Country service error - {}", result.getBody());
+        } else {
+          try {
+            String json = result.asJson().toString();
+            return objectMapper.readValue(json, new TypeReference<List<Country>>() {
+            });
+          } catch (IOException e) {
+            Logger.error("Failed to parse Country service response as JSON.", e);
           }
+        }
 
-          return new ArrayList<>();
-        }, httpExecutionContext.current());
+        return new ArrayList<>();
+      }, httpExecutionContext.current());
   }
 
 }
