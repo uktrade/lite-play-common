b2e40667a251436601ff23cf597415b9adf4e497
Stefan Brach <brach.stefan@gmail.com>
2018-04-30 17:02:29 +0100

    LITE-1064 Changed notification service client implementation from rest to sqs

--- app/components/common/client/NotificationServiceClient.java
+++ app/components/common/client/NotificationServiceClient.java
@@ -1,87 +1,53 @@
 package components.common.client;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
-import components.common.logging.CorrelationId;
-import play.Logger;
-import play.libs.Json;
-import play.libs.concurrent.HttpExecutionContext;
-import play.libs.ws.WSClient;
-import play.libs.ws.WSResponse;
+import org.slf4j.LoggerFactory;
+import uk.gov.bis.lite.notification.api.EmailNotification;
 
-import java.time.Duration;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 
 public class NotificationServiceClient {
 
-  private final HttpExecutionContext httpExecutionContext;
-  private final WSClient ws;
-  private final int wsTimeout;
-  private final String wsUrl;
-  private final String credentials;
+  private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(NotificationServiceClient.class);
+  private static final ObjectMapper MAPPER = new ObjectMapper();
 
-  private static final String TEMPLATE_QUERY_NAME = "template";
-  private static final String RECIPIENT_EMAIL_QUERY_NAME = "recipientEmail";
+  private final AmazonSQS amazonSQS;
+  private final String notificationServiceAwsSqsQueueUrl;
 
   @Inject
-  public NotificationServiceClient(HttpExecutionContext httpExecutionContext,
-                                   WSClient ws,
-                                   @Named("notificationServiceAddress") String wsAddress,
-                                   @Named("notificationServiceTimeout") int wsTimeout,
-                                   @Named("notificationServiceCredentials") String credentials) {
-    this.httpExecutionContext = httpExecutionContext;
-    this.ws = ws;
-    this.wsTimeout = wsTimeout;
-    this.wsUrl = wsAddress + "/notification/send-email";
-    this.credentials = credentials;
+  public NotificationServiceClient(@Named("notificationServiceAwsSqsQueueUrl") String notificationServiceAwsSqsQueueUrl,
+                                   AmazonSQS amazonSQS) {
+    this.notificationServiceAwsSqsQueueUrl = notificationServiceAwsSqsQueueUrl;
+    this.amazonSQS = amazonSQS;
   }
 
   /**
    * Sends an email using the Notification service.
-   * @param templateName Template name of email to send. This must be registered on the Notification service.
-   * @param emailAddress Recipient email address.
-   * @param templateParams Name/value pairs of parameters for the given template. This map must match the parameter
+   *
+   * @param templateName    Template name of email to send. This must be registered on the Notification service.
+   * @param emailAddress    Recipient email address.
+   * @param personalisation Name/value pairs of parameters for the given template. This map must match the parameter
    */
-  public CompletionStage<Boolean> sendEmail(String templateName, String emailAddress, Map<String, String> templateParams) {
-
-    Logger.info("notification [" + templateName + "|" + emailAddress + "]");
-
-    ObjectNode nameValueJson = Json.newObject();
-    templateParams.forEach(nameValueJson::put);
-
-    CompletionStage<WSResponse> wsResponse = ws.url(wsUrl)
-        .setRequestFilter(CorrelationId.requestFilter)
-        .addHeader("Content-Type", "application/json")
-        .setRequestTimeout(Duration.ofMillis(wsTimeout))
-        .addQueryParameter(TEMPLATE_QUERY_NAME, templateName)
-        .addQueryParameter(RECIPIENT_EMAIL_QUERY_NAME, emailAddress)
-        .setAuth(credentials)
-        .post(nameValueJson);
-
-    CompletionStage<Boolean> result = wsResponse.thenComposeAsync(r -> {
-      Logger.info("notification [response status: " + r.getStatus() + "]");
-      if (r.getStatus() == 200) {
-        return CompletableFuture.supplyAsync(() -> true);
-      } else {
-        return CompletableFuture.supplyAsync(() -> false);
-      }
-    }, httpExecutionContext.current());
-
-    return result.handleAsync((responseIsOk, error) -> {
-      if (error != null || !responseIsOk) {
-        Logger.error("Notification service failure");
-        if (error != null) {
-          Logger.error(error.getMessage(), error);
-        }
-        return false;
-      }
-      else {
-        return true;
-      }
-    }, httpExecutionContext.current());
+  public void sendEmail(String templateName, String emailAddress, Map<String, String> personalisation) {
+    EmailNotification emailNotification = new EmailNotification();
+    emailNotification.setTemplate(templateName);
+    emailNotification.setEmail(emailAddress);
+    emailNotification.setPersonalisation(personalisation);
+    String message;
+    try {
+      message = MAPPER.writeValueAsString(emailNotification);
+    } catch (JsonProcessingException jpe) {
+      throw new RuntimeException("Unable to write email notification as string ", jpe);
+    }
+    try {
+      amazonSQS.sendMessage(notificationServiceAwsSqsQueueUrl, message);
+    } catch (Exception exception) {
+      LOGGER.error("Unable to send message {}", message);
+    }
   }
 
 }
