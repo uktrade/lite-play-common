c67594a22c37b2fe8d7ce2f1da7c8c58d220b76f
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2018-07-18 17:55:02 +0100

    Country cache actor now backs off load times exponentially, with a maximum

--- app/components/common/cache/UpdateCountryCacheActor.java
+++ app/components/common/cache/UpdateCountryCacheActor.java
@@ -6,39 +6,71 @@ import akka.actor.UntypedAbstractActor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
 
 import java.util.concurrent.TimeUnit;
 
 public class UpdateCountryCacheActor extends UntypedAbstractActor {
+  public static final String LOAD_MESSAGE = "load";
+  public static final String STOP_MESSAGE = "stop";
+
   private static final Logger LOGGER = LoggerFactory.getLogger(UpdateCountryCacheActor.class);
 
   private final CountryProvider countryProvider;
   private final int maxAttempts;
+  private final int maxDelaySeconds;
+  private final int[] lookupDelaySeconds;
+
   private int attempts;
 
-  public UpdateCountryCacheActor(CountryProvider countryProvider, int maxAttempts) {
+  public UpdateCountryCacheActor(CountryProvider countryProvider, int maxAttempts, int maxDelaySeconds) {
     this.countryProvider = countryProvider;
     this.maxAttempts = maxAttempts;
+    this.maxDelaySeconds = maxDelaySeconds;
+    this.lookupDelaySeconds = initializeDelayLookup(maxAttempts);
   }
 
-    @Override
+  @Override
   public void onReceive(Object message) throws Exception {
-    if ("load".equals(message)) {
-      if (attempts < maxAttempts) {
-        ActorSystem system = getContext().getSystem();
-        system.scheduler().scheduleOnce(Duration.create(attempts + 1, TimeUnit.SECONDS), () -> {
-          try {
-            countryProvider.loadCountries().toCompletableFuture().get();
-          } catch (Exception e) {
-            LOGGER.error("Error during country provider load", e);
-            self().tell("load", ActorRef.noSender());
-          }
-        }, system.dispatcher());
-        attempts++;
-      } else {
-        LOGGER.error("Failed to load country cache after {} attempts", attempts);
-        attempts = 0;
-      }
+    if (LOAD_MESSAGE.equals(message)) {
+      attempts++;
+      ActorSystem system = getContext().getSystem();
+      FiniteDuration delay = delayDuration(attempts);
+      LOGGER.info("Scheduling cache load for delay of {} second(s)", delay.toSeconds());
+      system.scheduler().scheduleOnce(delay, () -> {
+        try {
+          countryProvider.loadCountries().toCompletableFuture().get();
+          self().tell(STOP_MESSAGE, ActorRef.noSender());
+          LOGGER.info("Successful cache load after {} attempts", attempts);
+        } catch (Exception e) {
+          LOGGER.error("Error during country provider load", e);
+          self().tell(LOAD_MESSAGE, ActorRef.noSender());
+        }
+      }, system.dispatcher());
+    } else if (STOP_MESSAGE.equals(message)) {
+      LOGGER.info("Stopped scheduling cache loads after {} attempts", attempts);
+      attempts = 0;
+    } else {
+      LOGGER.error("Unknown message value {}", message);
     }
+  }
+
+  private FiniteDuration delayDuration(int attempt) {
+    if (attempt <= maxAttempts && attempt <= lookupDelaySeconds.length && attempt > 0 && lookupDelaySeconds[attempt -1] <= maxDelaySeconds) {
+      return Duration.create(lookupDelaySeconds[attempt - 1], TimeUnit.SECONDS);
+    } else {
+      return Duration.create(maxDelaySeconds, TimeUnit.SECONDS);
+    }
+  }
+
+  private int[] initializeDelayLookup(int total) {
+    // Initialize array with fibonacci sequence of length total
+    int[] delayLookup = new int[total];
+    delayLookup[0] = 0;
+    delayLookup[1] = 1;
+    for(int i = 2; i < total; i++){
+      delayLookup[i] = delayLookup[i - 1] + delayLookup[i - 2];
+    }
+    return delayLookup;
   }
 }
